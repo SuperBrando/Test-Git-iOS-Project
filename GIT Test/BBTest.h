31643cc41e1ac341215958249c4d269fa6adbbb7
Brandon Byus <bsquared95@Brandons-iMac.local>
2014-05-28 13:42:29 -0700

    added test class

---
+++ GIT Test/BBTest.h
@@ -0,0 +1,13 @@
+//
+//  BBTest.h
+//  GIT Test
+//
+//  Created by Brandon Byus on 5/28/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BBTest : NSObject
+
+@end
---
+++ GIT Test/BBTest.m
@@ -0,0 +1,13 @@
+    //
+//  BBTest.m
+//  GIT Test
+//
+//  Created by Brandon Byus on 5/28/14.
+//
+//
+
+#import "BBTest.h"
+
+@implementation BBTest
+
+@end
